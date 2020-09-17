9f01f8d695e14ad1c79da8d6a97d126f464e0808
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-17 18:19:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_value = 20000
-  key_for_min_value = ""
+  key_for_min_value = nil
   name_hash.each do |key, value|
     if value < min_value
       min_value = value
