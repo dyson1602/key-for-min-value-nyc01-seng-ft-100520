d5b0cdd0be33b7397ba4ba3102499a9cd2032644
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-17 18:13:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  min_value = 20000
   name_hash.each do |key, value|
-    if 
+    if value < min_value
+      min_value == value
+    end
+  end
+  
   
 end
